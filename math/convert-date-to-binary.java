9ab92b02d8ba11a5219e0913c3108ad70caa9277
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-22 13:17:42 +0530

    LC-convert-date-to-binary [Runtime: N/A; Memory: N/A]

---
+++ math/convert-date-to-binary.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+       String parts[] = date.split("-");
+       StringBuilder ans = new StringBuilder();
+       for(int i =0 ;i<parts.length;i++){
+           int num = Integer.parseInt(parts[i]);
+           ans.append(Integer.toBinaryString(num));
+           
+           if(i != parts.length-1){
+            ans.append("-");
+           }
+         
+       }
+       return ans.toString();
+    }
+}
