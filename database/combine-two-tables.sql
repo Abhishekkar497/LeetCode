7c86dd047dea720aa7c14bea9d2beacc0171e335
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-27 00:44:49 +0530

    LC-combine-two-tables [Runtime: N/A; Memory: N/A]

--- database/combine-two-tables.sql
+++ database/combine-two-tables.sql
@@ -1,5 +1,4 @@
-SELECT p.firstName,p.lastName,
-(SELECT city FROM address a WHERE a.personId = p.personid) as city,
-(SELECT state FROM address a1 Where a1.personId= p.personid) as state
-
-FROM person  p;
+# Write your MySQL query statement below
+SELECT p.firstName,p.lastName,a.city ,a.state from person p
+LEFT JOIN Address A 
+ON p.personId = a.personId;
