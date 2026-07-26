8d1d2b77a09bc3718c6d149a1a561e90ebcedd58
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-26 18:24:37 +0530

    LC-build-array-from-permutation [Runtime: N/A; Memory: N/A]

--- simulation/build-array-from-permutation.java
+++ simulation/build-array-from-permutation.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-    
-        int[] ans = new int[nums.length];
+        
+        int ans[] = new int[nums.length];
         for(int i =0;i<nums.length;i++){
-            int curr = nums[i];
-            ans[i] = nums[curr];
+            ans[i] = nums[nums[i]];
+            
         }
         return ans;
     }
