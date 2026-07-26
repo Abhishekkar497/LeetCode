c031f4ff39ddd334753f92037a08d3031aecd729
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-27 00:31:33 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock.java
+++ array/best-time-to-buy-and-sell-stock.java
@@ -2,10 +2,13 @@ class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
         int min = prices[0];
-        for(int i=1;i<prices.length;i++){
-           min = Math.min(prices[i],min);
-           profit = Math.max(profit,prices[i] -min);
-
+        for(int i =1;i<prices.length;i++){
+            if(prices[i] <min){
+               min = prices[i];
+            }
+            else if(prices[i] - min > profit ){
+                profit = prices[i] -min;
+            }
         }
         return profit;
       
