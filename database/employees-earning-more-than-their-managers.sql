7725cf5f406fc0eb9adf18ceb318f0bb9036bd40
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-28 00:29:13 +0530

    LC-employees-earning-more-than-their-managers [Runtime: N/A; Memory: N/A]

--- database/employees-earning-more-than-their-managers.sql
+++ database/employees-earning-more-than-their-managers.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT e.name AS Employee from Employee e WHERE e.salary >(SELECT m.salary from Employee m  where e.managerid = m.id);
+SELECT e.name AS Employee from employee e where e.Salary > (SELECT salary from Employee where e.managerId = id);
