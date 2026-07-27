1ee6ecac77a29438ab7524d4e4d5c72414a180df
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-27 17:41:46 +0530

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- two-pointers/move-zeroes.java
+++ two-pointers/move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                nums[index] = nums[i];
-                index++;
+        int slow =0;
+        for(int fast = 0;fast <nums.length;fast++){
+            if(nums[fast]!= 0){
+                nums[slow] = nums[fast];
+                slow++;
             }
-    
+
         }
-        for(int i = index;i<nums.length;i++){
-            nums[i] =0;
+
+        while(slow < nums.length){
+            nums[slow] = 0;
+            slow++;
         }
     }
 }
