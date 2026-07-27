0c9b4787d2e7373665a477064961b2a7401fbb4a
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-28 00:24:42 +0530

    LC-nth-highest-salary [Runtime: N/A; Memory: N/A]

--- database/nth-highest-salary.sql
+++ database/nth-highest-salary.sql
@@ -1,9 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-DECLARE M INT;
-  SET M = N - 1;
+    declare m int;
+    set m=N-1;
   RETURN (
-    -- select distinct salary from employee order by salary desc limit 1 offset M
-      select distinct salary from employee e1 where (select count(distinct salary) from employee e2 where e1.salary < e2.salary) = N-1
+    --   select distinct salary from employee e1 where (select count(distinct salary) from employee e2 where e1.salary <= e2.salary) = N
+    select distinct salary from employee order by salary desc limit 1 offset M
   );
 END
