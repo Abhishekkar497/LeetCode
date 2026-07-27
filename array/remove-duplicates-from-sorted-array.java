2734c96067814caae562f7e9e667c8a4c0921318
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-07-27 19:50:31 +0530

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

--- array/remove-duplicates-from-sorted-array.java
+++ array/remove-duplicates-from-sorted-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 0;
-       
-        for(int i =1;i<nums.length;i++){
+        int slow = 0;
+        for(int fast = 1;fast < nums.length;fast++){
+            if(nums[fast] != nums[slow]){
             
-            if(nums[k] != nums[i]){
-             k++;
-            nums[k] = nums[i];
+                slow++;
+                nums[slow] = nums[fast];
             }
-            
         }
-        return k+1;
+        return slow+1;
     }
 }
