0307cf9f97b3915a418cfeef1d7b7ad2d3db5681
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-02 00:58:10 +0530

    LC-maximum-average-subarray-i [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-average-subarray-i.java
+++ sliding-window/maximum-average-subarray-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int sum = 0;
-        double maxSum = 0;
+        int maxSum = Integer.MAX_VALUE;
         for(int i =0;i<k;i++){
             sum+=nums[i];
         }
