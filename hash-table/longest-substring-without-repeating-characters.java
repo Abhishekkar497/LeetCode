3799af45782510a034ffd916c7fe361713a48b0b
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-02 20:20:11 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-substring-without-repeating-characters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLen = 0;
+        int l =0,r=0;
+        int hash[] = new int[256];
+         Arrays.fill(hash,0);
+         int n =s.length();
+         while(r < n){
+            if(hash[s.charAt(r)] >=l){
+              l = Math.max(hash[s.charAt(r)]+1,l);  
+            }
+            int len = r-l + 1;
+            maxLen = Math.max(len,maxLen);
+            hash [s.charAt(r)] = r;
+            r++;
+         }
+        return maxLen;
+
+
+    }
+}
