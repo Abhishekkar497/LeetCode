30b77d37002c22e80472ac7c5b3869f0a0259bfb
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-02 17:36:31 +0530

    LC-employee-bonus [Runtime: N/A; Memory: N/A]

--- database/employee-bonus.sql
+++ database/employee-bonus.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-SELECT e.name,b.bonus FROM Employee e
-LEFT JOIN Bonus b
-ON e.empid = b.empid
-WHERE bonus <1000
-OR 
-Bonus IS NULL;
+SELECT e.name, b.bonus from Employee e
+LEFT Join Bonus b ON b.empid = e.empid
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
