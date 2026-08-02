02cd67a524d4e945bcd6d0961cd12a35939407ec
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-02 20:32:38 +0530

    LC-longest-substring-without-repeating-characters [Runtime: 9 ms; Memory: 47.6 MB]

--- sliding-window/longest-substring-without-repeating-characters.java
+++ sliding-window/longest-substring-without-repeating-characters.java
@@ -1,21 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int maxLen = 0;
+        int[] hash = new int[256];
+        Arrays.fill(hash,-1);
+        int n = s.length();
         int l =0,r=0;
-        int hash[] = new int[256];
-         Arrays.fill(hash,-1);
-         int n =s.length();
-         while(r < n){
-            if(hash[s.charAt(r)] >=l){
-              l = Math.max(hash[s.charAt(r)]+1,l);  
+        while(r < n){
+            if(hash[s.charAt(r)] >= -1){
+                l = Math.max(hash[s.charAt(r)] +1,l);
             }
-            int len = r-l + 1;
+            int len = r-l+1;
             maxLen = Math.max(len,maxLen);
-            hash [s.charAt(r)] = r;
+            hash[s.charAt(r)] = r;
             r++;
-         }
+        }
         return maxLen;
-
-
     }
 }
