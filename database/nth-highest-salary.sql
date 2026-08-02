bc24e21d1f4033542417e16c162a5b152ad6232e
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-02 17:47:26 +0530

    LC-nth-highest-salary [Runtime: N/A; Memory: N/A]

--- database/nth-highest-salary.sql
+++ database/nth-highest-salary.sql
@@ -2,6 +2,6 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-     SELECT Salary from employee where salary < (Select Max(salary) FROM Employee Limit 1 offset 1 ) = N
+     SELECT Distinct e1.Salary from employee e1 where (Select Count( e2.salary)  FROM Employee e2 Where e1.salary <= e2.salary ) = N
   );
 END
