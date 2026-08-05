bf0e08865643f32cad26393f0a139817630f3d23
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-05 16:10:38 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- prefix-sum/product-of-array-except-self.java
+++ prefix-sum/product-of-array-except-self.java
@@ -2,20 +2,30 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int ans[] = new int[nums.length];
         int mul = 1;
+        int zeroCount = 0;
         for(int i =0;i<nums.length;i++){
             if(nums[i] !=0){
             mul*= nums[i];
+            }else {
+                zeroCount++;
             }
         }
         System.out.println(mul);
         for(int i =0;i<nums.length;i++){
-            if(nums[i] == 0){
-                ans[i] = mul;
-            }
-            else if(nums[i] != 0 && mul == 0){
+            if(zeroCount > 1){
                 ans[i] = 0;
-            }else {
-            ans[i] =  Math.abs(mul/nums[i]);
+            }
+            else if(zeroCount ==1){
+                if(nums[i] ==0){
+                    ans[i] = mul;
+                }
+                else {
+                    ans[i] = 0;
+                }
+            }
+            else
+             {
+            ans[i] =  (mul/nums[i]);
             }
         }
         return ans;
