771af0133679dd348a403f9f5327a11e915f838b
Abhishekkar497 <112310310+Abhishekkar497@users.noreply.github.com>
2026-08-07 16:08:42 +0530

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

--- binary-search/find-peak-element.java
+++ binary-search/find-peak-element.java
@@ -1,16 +1,15 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int low = 0;
-        int high = nums.length-1;
-        while(low <high){
-          int mid = low + (high  - low)/2;
-            if(nums[mid] <nums[mid+1]){
-                low = mid+1;
-            }
-            else{
+        int high = nums.length -1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] < nums[mid + 1]){
+                low = mid +1;
+            } else {
                 high = mid;
             }
         }
-        return low;
+        return high;
     }
 }
